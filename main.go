ff594366184b5f8748eef96470cbe54cb1a2a96f
Dan Kleiman <info@dankleiman.com>
2016-05-05 16:26:33 -0400

    use dotenv to load variables

--- main.go
+++ main.go
@@ -5,35 +5,45 @@ import(
   "os"
   "fmt"
   "gopkg.in/gomail.v2"
+  "github.com/joho/godotenv"
 )
+
 func main() {
+  err := godotenv.Load()
+  if err != nil {
+    panic("Error loading .env file")
+  }
+
   http.HandleFunc("/", hello)
   http.HandleFunc("/contact", contact)
+
   port := os.Getenv("PORT")
 
   if port == "" {
-    panic("$PORT must be set")
+    panic("PORT must be set")
   }
+
   http.ListenAndServe(":" + port, nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request){
   w.Write([]byte("hello!"))
 }
+
 func contact(w http.ResponseWriter, r *http.Request){
     r.ParseForm()
-    c := fmt.Sprintf("Hi %s\n We've recieved a message from %s:\n %s \nThanks!", r.FormValue("name"), r.FormValue("email"), r.FormValue("message"))
-
     email := os.Getenv("EMAIL")
 
-    // set details to recipient to say we got their submission
-    confirmation := new(mailDetails)
-    confirmation.To = r.FormValue("email")
-    confirmation.From = email
-    confirmation.Subject = "Thanks for your message!"
-    confirmation.Body = c
+    // c := fmt.Sprintf("Hi %s\n We've recieved a message from %s:\n %s \nThanks!", r.FormValue("name"), r.FormValue("email"), r.FormValue("message"))
 
-    confirmation.sendMail()
+    // // set details to recipient to say we got their submission
+    // confirmation := new(mailDetails)
+    // confirmation.To = r.FormValue("email")
+    // confirmation.From = email
+    // confirmation.Subject = "Thanks for your message!"
+    // confirmation.Body = c
+
+    // confirmation.sendMail()
 
     // send me the submission
     s := fmt.Sprintf("You've recieved a message from %s(%s):\n %s", r.FormValue("name"), r.FormValue("email"), r.FormValue("message"))
@@ -45,27 +55,27 @@ func contact(w http.ResponseWriter, r *http.Request){
     submission.Body = s
 
     submission.sendMail()
+}
+
+type mailDetails struct {
+  To string
+  From string
+  Subject string
+  Body string
+}
+
+func (m mailDetails) sendMail() int {
+  g := gomail.NewMessage()
+  g.SetHeader("From", m.From)
+  g.SetHeader("To", m.To)
+  g.SetHeader("Subject", m.Subject)
+  g.SetBody("text/html", m.Body)
+
+  d := gomail.NewDialer(os.Getenv("MAILGUN_SMTP_SERVER"), 587, os.Getenv("MAILGUN_SMTP_LOGIN"), os.Getenv("MAILGUN_SMTP_PASSWORD"))
+
+  if err := d.DialAndSend(g); err != nil {
+    panic(err)
   }
 
-  type mailDetails struct {
-    To string
-    From string
-    Subject string
-    Body string
-  }
-
-  func (m mailDetails) sendMail() int {
-    g := gomail.NewMessage()
-    g.SetHeader("From", m.From)
-    g.SetHeader("To", m.To)
-    g.SetHeader("Subject", m.Subject)
-    g.SetBody("text/html", m.Body)
-
-    d := gomail.NewDialer(os.Getenv("MAILGUN_SMTP_SERVER"), 587, os.Getenv("MAILGUN_SMTP_LOGIN"), os.Getenv("MAILGUN_SMTP_PASSWORD"))
-
-    if err := d.DialAndSend(g); err != nil {
-      panic(err)
-    }
-
-    return 0
-  }
+  return 0
+}
