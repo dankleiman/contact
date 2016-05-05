c9a8eca07a59f8d0efd6dc80cd0f8206c498c569
Dan Kleiman <info@dankleiman.com>
2016-05-05 16:04:02 -0400

    add mailgun

--- main.go
+++ main.go
@@ -2,54 +2,49 @@ package main
 
 import(
   "net/http"
-  // "log"
-  // "os"
+  "os"
   "fmt"
   "gopkg.in/gomail.v2"
 )
 func main() {
   http.HandleFunc("/", hello)
   http.HandleFunc("/contact", contact)
-  // do we serve off an enviroment variable here?
-  http.ListenAndServe(":8080", nil)
+  port := os.Getenv("PORT")
+
+  if port == "" {
+    panic("$PORT must be set")
+  }
+  http.ListenAndServe(":" + port, nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request){
   w.Write([]byte("hello!"))
 }
 func contact(w http.ResponseWriter, r *http.Request){
-    // f, err := os.OpenFile("log.txt", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0666)
-    // if err != nil {
-    //   log.Fatal(err)
-    // }
-
-    // defer f.Close()
-    // log.SetOutput(f)
-
     r.ParseForm()
     c := fmt.Sprintf("Hi %s\n We've recieved a message from %s:\n %s \nThanks!", r.FormValue("name"), r.FormValue("email"), r.FormValue("message"))
+
+    email := os.Getenv("EMAIL")
 
     // set details to recipient to say we got their submission
     confirmation := new(mailDetails)
     confirmation.To = r.FormValue("email")
-    confirmation.From = "info@dankleiman.com"
+    confirmation.From = email
     confirmation.Subject = "Thanks for your message!"
     confirmation.Body = c
 
-    // log.Println(confirmation.Body)
-    // confirmation.sendMail()
+    confirmation.sendMail()
 
-    // // send me the submission
+    // send me the submission
     s := fmt.Sprintf("You've recieved a message from %s(%s):\n %s", r.FormValue("name"), r.FormValue("email"), r.FormValue("message"))
 
     submission := new(mailDetails)
-    submission.To = "info@dankleiman.com"
+    submission.To = email
     submission.From = r.FormValue("email")
     submission.Subject = "New contact form submission"
     submission.Body = s
 
-    // log.Println(submission.Body)
-    // submission.sendMail()
+    submission.sendMail()
   }
 
   type mailDetails struct {
@@ -60,14 +55,13 @@ func contact(w http.ResponseWriter, r *http.Request){
   }
 
   func (m mailDetails) sendMail() int {
-    // Set up authentication information.
     g := gomail.NewMessage()
     g.SetHeader("From", m.From)
     g.SetHeader("To", m.To)
     g.SetHeader("Subject", m.Subject)
     g.SetBody("text/html", m.Body)
 
-    d := gomail.NewDialer("smtp.example.com", 587, "user", "123456")
+    d := gomail.NewDialer(os.Getenv("MAILGUN_SMTP_SERVER"), 587, os.Getenv("MAILGUN_SMTP_LOGIN"), os.Getenv("MAILGUN_SMTP_PASSWORD"))
 
     if err := d.DialAndSend(g); err != nil {
       panic(err)
